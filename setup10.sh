581491959bd8539433285886f35048d7fb3d77c9
root <root@localhost.localdomain>
2021-01-26 02:58:43 -0500

    this is real

---
+++ setup10.sh
@@ -0,0 +1,4 @@
+VARS1="HOME=|USER=|MAIL=|LC_ALL=|LS_COLORS=|LANG="
+VARS2="HOSTNAME=|PWD=|TERM=|SHLVL=|LANGUAGE=|_="
+VARS="${VARS1}|${VARS2}"
+docker exec agent sh -c "env | egrep -v '^(${VARS})' >> /etc/environment"
